6d48574d2beb62d2d77bd718cde7a975f14022d1
cemilture <cemilture@gmail.com>
2023-11-22 14:06:13 +0300

    css editing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Quote from "./assets/Quote";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BsQuote } from "react-icons/bs";
 
 interface QuoteData {
   content: string;
@@ -22,17 +23,15 @@ function App() {
   }, []);
 
   return (
-    <div className="grid text-center mt-5">
-      <div className="quote-container p-4 position-relative">
+    <div className="quote-container p-3 d-flex flex-column align-content-center">
+      <div className="fs-3 d-flex flex-row">
+        <BsQuote size={"150px"} />
         <Quote text={quote.content} author={quote.author} />
-        <div
-          className="position-absolute bottom-0 end-0"
-          style={{ margin: "10px" }}
-        >
-          <button className="btn btn-primary" onClick={fetchNewQuote}>
-            New Quote !
-          </button>
-        </div>
+      </div>
+      <div>
+        <button className="btn btn-primary" onClick={fetchNewQuote}>
+          New Quote !
+        </button>
       </div>
     </div>
   );
