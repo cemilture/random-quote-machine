7641d237081ac7781c1dbb35d5af10b66caa7958
cemilture <cemilture@gmail.com>
2023-11-22 14:10:11 +0300

    deleted:    src/App.css 	modified:   src/App.tsx 	modified:   src/assets/Quote.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
-import "./App.css";
 import Quote from "./assets/Quote";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BsQuote } from "react-icons/bs";
+
+import { ImFacebook2 } from "react-icons/im";
+import { FaXTwitter } from "react-icons/fa6";
 
 interface QuoteData {
   content: string;
@@ -22,17 +23,63 @@ function App() {
     fetchNewQuote();
   }, []);
 
+  const shareOnTwitter = () => {
+    const tweetText = `"${quote.content}" - ${quote.author}`;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
+      tweetText
+    )}`;
+    window.open(twitterUrl, "_blank");
+  };
+
+  const shareOnFacebook = () => {
+    const facebookUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+      window.location.href
+    )}`;
+    window.open(facebookUrl, "_blank");
+  };
+
   return (
-    <div className="quote-container p-3 d-flex flex-column align-content-center">
-      <div className="fs-3 d-flex flex-row">
-        <BsQuote size={"150px"} />
-        <Quote text={quote.content} author={quote.author} />
+    <div
+      className="fill d-flex flex-column justify-content-center align-items-center vh-100 "
+      style={{ backgroundColor: "bisque" }}
+    >
+      <div
+        style={{ maxWidth: "600px", backgroundColor: "gainsboro" }}
+        className="card p-3"
+      >
+        <div className="d-flex flex-row ">
+          <p className="fw-bold fs-2">
+            <Quote text={quote.content} author={quote.author} />
+          </p>
+        </div>
+        <div className="d-flex justify-content-between">
+          <div>
+            <button
+              style={{ border: "none", backgroundColor: "transparent" }}
+              onClick={shareOnTwitter}
+              data-bs-toggle="tooltip"
+              data-bs-placement="bottom"
+              title="Share on Twitter !"
+            >
+              <FaXTwitter size={"50px"} />
+            </button>
+            <button
+              style={{ border: "none", backgroundColor: "transparent" }}
+              onClick={shareOnFacebook}
+              data-bs-toggle="tooltip"
+              data-bs-placement="bottom"
+              title="Share on Facebook !"
+            >
+              <ImFacebook2 size={"50px"} />
+            </button>
+          </div>
+
+          <button className="btn btn-dark" onClick={fetchNewQuote}>
+            New Quote !
+          </button>
+        </div>
       </div>
-      <div>
-        <button className="btn btn-primary" onClick={fetchNewQuote}>
-          New Quote !
-        </button>
-      </div>
+      by cemilture
     </div>
   );
 }
--- src/assets/Quote.tsx
+++ src/assets/Quote.tsx
@@ -1,3 +1,4 @@
+import { BsQuote } from "react-icons/bs";
 interface QuoteProps {
   text: string;
   author: string;
@@ -6,8 +7,11 @@ interface QuoteProps {
 const Quote: React.FC<QuoteProps> = ({ text, author }) => {
   return (
     <div>
-      <p>{text}</p>
-      <p>{author}</p>
+      <p className="text-center">
+        <BsQuote size={"45px"} />
+        {text}
+      </p>
+      <p className="text-end fst-italic fs-4 fw-light">~ {author}</p>
     </div>
   );
 };
